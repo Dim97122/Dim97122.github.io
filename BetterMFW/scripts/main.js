3184ae1c8c6fd84a28005201476868840b1b6e3f
Dim <dimitri.kiavue@gmail.com>
2018-06-24 19:45:43 +0200

    Timer Popup

---
+++ BetterMFW/scripts/main.js
@@ -0,0 +1,12 @@
+
+function() { alert("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please"); }, 1000);
+
+var myPopup;
+
+function () {
+    myPopup = setTimeout(alertXXX, 10000);
+}
+
+function alertXXX() {
+    alert("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please");
+}
