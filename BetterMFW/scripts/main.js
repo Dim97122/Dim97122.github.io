c40f49dfbb9722860a5700d3f6a6b7036b8698b1
Dim <dimitri.kiavue@gmail.com>
2018-06-24 19:48:59 +0200

    ...

--- BetterMFW/scripts/main.js
+++ BetterMFW/scripts/main.js
@@ -1,12 +1,10 @@
 
-function() { alert("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please"); }, 1000);
+var myVar;
 
-var myPopup;
-
-function () {
-    myPopup = setTimeout(alertXXX, 10000);
+function myFunction() {
+    myVar = setTimeout(alertFunc, 3000);
 }
 
-function alertXXX() {
-    alert("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please");
+function alertFunc() {
+    alert("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please!");
 }
