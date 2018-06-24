ee602340c99178b1814be87ae98ddabd2125886b
Dim <dimitri.kiavue@gmail.com>
2018-06-24 13:11:51 +0200

    Bouton de Nom + Enregistrment Navi

--- JS/beginner-html-site-styled-gh-pages/scripts/main.js
+++ JS/beginner-html-site-styled-gh-pages/scripts/main.js
@@ -1,5 +1,3 @@
-let monTitre = document.querySelector('h1');
-monTitre.textContent = 'Hello world!';
 
 let testCond = 'mozilla';
 if (testCond='mozilla') {
@@ -13,6 +11,39 @@ function testFunction(num1,num2) {
   return résultat;
 }
 
-document.querySelector('html').onclick = function testEvent() {
+/*
+document.querySelector('html').onclick = function() {
     alert('Aïe, arrêtez de cliquer !!');
+}
+*/
+var monImage = document.querySelector('img');
+
+monImage.onclick = function() {
+    var monSrc = monImage.getAttribute('src');
+    if(monSrc === 'images/firefox-icon.png') {
+      monImage.setAttribute ('src','images/google-icon.png');
+    } else {
+      monImage.setAttribute ('src','images/firefox-icon.png');
+    }
+}
+
+var monBouton = document.querySelector('button');
+
+var monTitre = document.querySelector('h1');
+
+function définirNomUtilisateur() {
+  var monNom = prompt('Veuillez saisir votre nom.');
+  localStorage.setItem('nom', monNom);
+  monTitre.textContent = 'Mozilla est cool, ' + monNom;
+}
+
+if(!localStorage.getItem('nom')) {
+  définirNomUtilisateur();
+} else {
+  let nomEnregistré = localStorage.getItem('nom');
+  monTitre.textContent = 'Mozilla est cool, ' + nomEnregistré;
+}
+
+monBouton.onclick = function() {
+  définirNomUtilisateur();
 }
