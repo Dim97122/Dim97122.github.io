b9849110eafedfef43c60ed181b1dca2c54fc507
Dim <dimitri.kiavue@gmail.com>
2018-06-24 20:35:09 +0200

    ...

--- BetterMFW/scripts/main.js
+++ BetterMFW/scripts/main.js
@@ -1,10 +1,9 @@
+var timeoutID;
 
-var myVar;
-
-function myFunction() {
-    myVar = setTimeout(alertFunc, 3000);
+function delayedAlert() {
+  timeoutID = window.setTimeout(slowAlert, 2000);
 }
 
-function alertFunc() {
-    alert("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please!");
+function slowAlert() {
+  alert('Hey le site xxxvidsxxx est trop bien. Viens dessus stp please');
 }
