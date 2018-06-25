bc61734a867274c60c1504eea734b7217ecda5c8
Dim <dimitri.kiavue@gmail.com>
2018-06-25 20:20:16 +0200

    Shitty setTimeout

--- BetterMFW/scripts/main.js
+++
@@ -1,9 +0,0 @@
-var timeoutID;
-
-function delayedAlert() {
-  timeoutID = window.setTimeout(slowAlert, 2000);
-}
-
-function slowAlert() {
-  alert('Hey le site xxxvidsxxx est trop bien. Viens dessus stp please');
-}
