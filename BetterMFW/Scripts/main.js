2df7d2bd9a5e7badb745401de98c3b9ef4727238
Dim <dimitri.kiavue@gmail.com>
2018-06-25 20:47:24 +0200

    PUREEEgit add .git add .git add .

--- BetterMFW/Scripts/main.js
+++ BetterMFW/Scripts/main.js
@@ -1,1 +1,8 @@
-setTimeout(function(){ alert("Hello"); }, 3000);
+var Nom, Prénom, Age, Identite;
+
+Nom = " " + prompt("Entrez votre Nom SVP");
+Prénom = "Je m'appelle " + prompt("Entrez votr Prénom SVP");
+Age = ", j'ai actuellement " + prompt ("Entrez votre Age SVP") + " ans.";
+Identite = Prénom + Nom + Age;
+
+alert(Identite);
