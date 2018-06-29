360f33b1feebeae682edaaf90ef92ac1aff30399
Dim <dimitri.kiavue@gmail.com>
2018-06-29 11:16:26 +0200

    Finalisation Better

--- BetterMFW/Scripts/main.js
+++ BetterMFW/Scripts/main.js
@@ -3,23 +3,10 @@ var test;
 test = setTimeout(function () {
   confirm("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please!!!");
 }, 10000);
-/*
-var magentaBack;
 
-magentaBack = setTimeout(function () {
-  var butMagenta = document.getElementById('magenta');
+monBouton = document.querySelector('button');
 
-  magenta.style.backgroundColor = "#ff00ff";
-}, 15000);
-
-function psychoColor() {
-  var changeHeading = document.getElementById('heading').style.color = "#008000";
-                      document.getElementById('heading').style.fontFamily ="Comic Sans MS";
-                      document.getElementById('heading2').style.color = "#008000";
-                      document.getElementById('heading2').style.fontFamily ="Comic Sans MS";
-  }
-*/
-
-function psychoColor() {
-  var changeHeading = document.getElementsByTagName("h1").style.color= "#008000";
+monBouton.onclick = function color()
+{
+	document.body.style.backgroundColor = "#FF00FF";
   }
